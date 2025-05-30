735eaee8cb9dbacffec93889741c4759be191a36
Victor Tahan <victortahan@gmail.com>
2025-05-30 15:31:13 -0300

    ajustes da porta para funcionar no Render

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get('/rodada/:numero', async (req, res) => {
   }
 })
 
-app.listen(8081, function(){
-    console.log('Servidor rodando na url http://localhost:8081')
+const PORT = process.env.PORT || 8081
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
